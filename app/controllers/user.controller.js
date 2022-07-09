29d799c56e70fae6ec67c8d27f2ddc08c5641614
Rodrigo Henrique <rodhenrc@outlook.com>
2022-07-09 17:54:15 -0300

    Novos controllers e rotas adicionadas

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,8 +1,21 @@
+const { Op } = require("sequelize");
 const { Autor, Editora, Livro } = require("../models");
 
 const getLivros = async (req, res) => {
   try {
-    const livros = await Livro.findAll({ include: Editora });
+    const livros = await Livro.findAll({
+      include: [
+        { model: Editora, attributes: ["nome"] },
+        { model: Autor, attributes: ["nome"] },
+      ],
+      attributes: [
+        "id",
+        "titulo",
+        "ano_lancamento",
+        "num_paginas",
+        "descricao",
+      ],
+    });
 
     if (!livros) return;
 
@@ -14,4 +27,120 @@ const getLivros = async (req, res) => {
   }
 };
 
-module.exports = { getLivros };
+const filterLivros = async (req, res) => {
+  //const { filtro, orderBy } = req.body;
+
+  const filtro = "ano_lancamento";
+  const orderBy = "ASC";
+
+  try {
+    if (filtro === "editora") {
+      const data = await Livro.findAll({
+        include: [
+          { model: Editora, attributes: ["nome"] },
+          { model: Autor, attributes: ["nome"] },
+        ],
+        attributes: [
+          "id",
+          "titulo",
+          "ano_lancamento",
+          "num_paginas",
+          "descricao",
+        ],
+        order: [[Editora, "nome", orderBy]],
+      });
+
+      if (!data) return;
+
+      res.status(200).json({
+        data,
+      });
+    } else if (filtro === "autor") {
+      const data = await Livro.findAll({
+        include: [
+          { model: Editora, attributes: ["nome"] },
+          { model: Autor, attributes: ["nome"] },
+        ],
+        attributes: [
+          "id",
+          "titulo",
+          "ano_lancamento",
+          "num_paginas",
+          "descricao",
+        ],
+        order: [[Autor, "nome", orderBy]],
+      });
+
+      if (!data) return;
+
+      res.status(200).json({
+        data,
+      });
+    } else if (
+      filtro === "titulo" ||
+      filtro === "ano_lancamento" ||
+      filtro === "num_paginas"
+    ) {
+      const data = await Livro.findAll({
+        include: [
+          { model: Editora, attributes: ["nome"] },
+          { model: Autor, attributes: ["nome"] },
+        ],
+        attributes: [
+          "id",
+          "titulo",
+          "ano_lancamento",
+          "num_paginas",
+          "descricao",
+        ],
+        order: [[filtro, orderBy]],
+      });
+
+      if (!data) return;
+
+      res.status(200).json({
+        data,
+      });
+    } else {
+      return;
+    }
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+};
+
+const search = async (req, res) => {
+  const termo = "mac";
+  try {
+    const data = await Livro.findAll({
+      where: {
+        [Op.or]: [
+          { titulo: { [Op.like]: `%${termo}%` } },
+          { "$Autor.nome$": { [Op.like]: `%${termo}%` } },
+        ],
+      },
+      include: [
+        { model: Editora, attributes: ["nome"] },
+        { model: Autor, attributes: ["nome"] },
+      ],
+
+      attributes: [
+        "id",
+        "titulo",
+        "ano_lancamento",
+        "num_paginas",
+        "descricao",
+      ],
+    });
+
+    if (!data) return;
+
+    res.status(200).json({
+      data,
+    });
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+};
+
+module.exports = { filterLivros, getLivros, search };
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { getLivros } = require("../controllers/user.controller");
+const {
+  filterLivros,
+  getLivros,
+  search,
+} = require("../controllers/user.controller");
 
 router.route("/").get(getLivros);
+router.route("/filtro").get(filterLivros);
+router.route("/search").get(search);
 
 module.exports = router;
